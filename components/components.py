e9a934e575c8b5542c36db0e8ad6f8f4594345cc
olgapotter <pink1234@mail.ru>
2025-04-29 19:17:03 +0300

    1

---
+++ components/components.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+class WebElement:
+    def __init__(self, driver, locator=''):
+        self.driver = driver
+        self.locator = locator
+
+    def click(self):
+        self.find_element().click()
+
+    def find_element(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.locator)
+
+    def exist(self):
+        try:
+            self.find_element()
+        except NoSuchElementException:
+            return False
+        return True
