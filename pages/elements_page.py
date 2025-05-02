e9b91cb8c22fec6bb099d7f358902f639c8b39eb
olgapotter <pink1234@mail.ru>
2025-05-02 17:31:58 +0300

    1

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -5,3 +5,4 @@ class ElementsPage(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
+        self.text_elements = WebElement(driver, 'div.col-12:nth-child(2)')
--- tests/test_check_text.py
+++ tests/test_check_text.py
@@ -1,5 +1,8 @@
 from pages.demoqa import DemoQa
 from components.components import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def test_check_center_text_on_elements_page(browser):
     demo_qa_page = DemoQa(browser)
@@ -12,9 +15,20 @@ def test_check_center_text_on_elements_page(browser):
     actual_url = browser.current_url
     assert actual_url == expected_url
 
+    # Ждём появления текста по центру
+    WebDriverWait(browser, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.playgound-body .col-md-6'))
+    )
+
     # Проверка текста по центру
-    center_text_element = WebElement(browser, '.playground-header div')
+    center_text_element = WebElement(browser, '.playgound-body .col-md-6')
     expected_text = 'Please select an item from left to start practice.'
     actual_text = center_text_element.get_text()
 
     assert actual_text == expected_text
+
+    def test_page_elements(browser):
+        el_page = ElementsPage(browser)
+
+        el_page.visit()
+        assert el_page.text.elements.get.text() == 'Please select an item from left to start practice.'
